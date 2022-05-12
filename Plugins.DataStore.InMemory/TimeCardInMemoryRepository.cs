d1bd71f4c6ae59510219dd7d5f21c68c6236ebaf
Krasimir Stoyanov <krasimir.stoyanov@bdo.global>
2022-05-12 09:36:11 +0300

    [Enhancement] - Rename solution, add /src folder

--- Plugins.DataStore.InMemory/TimeCardInMemoryRepository.cs
+++ Plugins.DataStore.InMemory/TimeCardInMemoryRepository.cs
@@ -12,8 +12,10 @@ namespace Plugins.DataStore.InMemory
         {
             timeCards = new List<TimeCard>()
             {
-                new TimeCard(){ Id = 1, UserName = "kstoyanov@mail.com", ProjectName = "Sundowning", TaskName = "Process Manual Documents in Databricks", Notes = "Add new notebook in Databricks - use python", Status = TimeCardStatus.New },
-                new TimeCard(){ Id = 2, UserName = "kstoyanov@mail.com", ProjectName = "Sundowning", TaskName = "Process GSA fles", Notes = "Extract attachments and upload them to azure blob storage - use dotnet", Status = TimeCardStatus.New},
+                new TimeCard(){ Id = 1, UserName = "Krasimir Stoyanov", ProjectName = "Sundowning", TaskName = "Process Manual Documents in Databricks", Notes = "Add new notebook in Databricks - use python", Status = TimeCardStatus.New },
+                new TimeCard(){ Id = 2, UserName = "Krasimir Stoyanov", ProjectName = "Sundowning", TaskName = "Process GSA fles", Notes = "Extract attachments and upload them to azure blob storage - use dotnet", Status = TimeCardStatus.New},
+                new TimeCard(){ Id = 3, UserName = "Krasimir Stoyanov", ProjectName = "Sundowning", TaskName = "Onboard newcomer", Notes = "Induction: ADF, ADO Pipeline, C# Solution", Status = TimeCardStatus.New},
+                new TimeCard(){ Id = 4, UserName = "Krasimir Stoyanov", ProjectName = "Sundowning", TaskName = "Bug fixing", Notes = "", Status = TimeCardStatus.New},
             };
         }
 
