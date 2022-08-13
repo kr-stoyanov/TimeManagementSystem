ae99df9c65983299a5290ce00126aba8ff897ec7
Krasimir Stoyanov <krasimir.stoyanov@bdo.global>
2022-08-13 11:30:36 +0300

    Refactor

--- src/WebApp/ViewModels/TimeCardViewModel.cs
+++ src/WebApp/ViewModels/TimeCardViewModel.cs
@@ -10,8 +10,6 @@ namespace WebApp.ViewModels
 
         public string? UserName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
         public string? ProjectName { get; set; }
 
         [Required]
@@ -21,6 +19,7 @@ namespace WebApp.ViewModels
         [MaxLength(2500)]
         public string? Notes { get; set; }
 
+        [Required]
         public TimeCardStatus Status { get; set; }
 
         public TimeSpan TimeSpent { get; set; }
